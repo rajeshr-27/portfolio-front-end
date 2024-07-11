543cb3010186f8864f21ad1afc8be2393472cf03
rajesh-27 <rajeshkphp27@gmail.com>
2024-07-11 20:47:55 +0530

    updated homepage content

--- src/components/Home.js
+++ src/components/Home.js
@@ -15,7 +15,7 @@ function Home() {
                             <div class="text-center text-xxl-start">
                                 <div class="badge bg-gradient-primary-to-secondary text-white mb-4"><div class="text-uppercase">A &middot; Networking &middot; Platform</div></div>
                                 <div class="fs-3 fw-light text-muted">BIGCONNECTS</div>
-                                <h1 class="display-3 fw-bolder mb-5"><span class="text-gradient d-inline">Get online and grow fast</span></h1>
+                                <h1 class="display-3 fw-bolder mb-5"><span class="text-gradient d-inline">Share your Bigconnects from your Instagram, Facebook, LinkedIn, and other bios.</span></h1>
                                 <div class="d-grid gap-3 d-sm-flex justify-content-sm-center justify-content-xxl-start mb-3">
                                     <Link class="btn btn-primary btn-lg px-5 py-3 me-sm-3 fs-6 fw-bolder" to="/register">Register</Link>
                                     <Link class="btn btn-outline-dark btn-lg px-5 py-3 fs-6 fw-bolder" to="/login">Login</Link>
